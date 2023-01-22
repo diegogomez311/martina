d413f790619c351e452cd26a91dbaa21ca0a78a6
“Diego <diego.zagga@gmail.com>
2023-01-21 21:35:56 -0300

    inicial

--- js/js.js
+++ js/js.js
@@ -1,43 +1,39 @@
-(function () {
-   const second = 1000,
-        minute = second * 60,
-        hour = minute * 60,
-        day = hour * 24;
+var countdown = document.querySelector('.countdown');
 
-  //I'm adding this section so I don't have to keep updating this pen every year :-)
-  //remove this if you don't need it
-   let today = new Date(),
-      dd = String(today.getDate()).padStart(2, "0"),
-      mm = String(today.getMonth() + 1).padStart(2, "0"),
-      yyyy = today.getFullYear(),
-      nextYear = yyyy + 1,
-      dayMonth = "06/16/",
-      birthday = dayMonth + yyyy;
 
-   today = mm + "/" + dd + "/" + yyyy;
-      if (today > birthday) {
-         birthday = dayMonth + nextYear;
-      }
-  //end
 
-   const countDown = new Date(birthday).getTime(),
-      x = setInterval(function() {    
+// Update every second
+var x = setInterval( function() {
+  // Set Launch Date (ms)
+var launchDate = new Date('Jul 29, 2023 21:00:00').getTime();
+  // Get todays date and time (ms)
+  var now = new Date().getTime();
 
-         const now = new Date().getTime(),
-               distance = countDown - now;
+  // Distance from now and the launch date (ms)
+  var distance = launchDate - now;
 
-         document.getElementById("days").innerText = Math.floor(distance / (day)),
-            document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-            document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-            document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+  // Time calculation
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  var mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-        //do something later when date is reached
-         if (distance < 0) {
-            document.getElementById("headline").innerText = "It's my birthday!";
-            document.getElementById("countdown").style.display = "none";
-            document.getElementById("content").style.display = "block";
-         clearInterval(x);
-         }
-        //seconds
-      }, 0)
-}());
+  // Display result
+  countdown.innerHTML = `
+  <div>${days}<span>Days</span></div> 
+  <div>${hours}<span>Hours</span></div>
+  <div>${mins}<span>Minutes</span></div>
+  <div>${seconds}<span>Seconds</span></div>
+  `;
+
+  // If launch date is reached
+  if (distance < 0) {
+    // Stop countdown
+    clearInterval(intvl);
+    // Style and output text
+    countdown.style.color = '#17a2b8';
+    countdown.innerHTML = 'Launched!';
+  }
+}, 1000);
